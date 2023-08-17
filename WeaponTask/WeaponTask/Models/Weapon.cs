2e2c937210a1bc417227622c48aa62985dd45402
ramalfatullayevh <tu7ma9clt@code.edu.az>
2023-08-17 23:46:30 +0400

    Weapon Final Commit

---
+++ WeaponTask/WeaponTask/Models/Weapon.cs
@@ -0,0 +1,162 @@
+﻿using System.ComponentModel.Design;
+
+namespace WeaponTask.Models
+{
+    internal class Weapon
+    {
+        int _bulletCapacity;
+        int _bulletCount;
+        int _dischargeTime;
+        bool _isAuto;
+
+
+        public int  BulletCapacity
+        {
+            get { return _bulletCapacity; }
+            set 
+            {
+                if (value>0)
+                {
+                    _bulletCapacity = value;
+                }
+            }
+        }
+
+        public int BulletCount
+        {
+            get { return _bulletCount; }
+            set
+            {
+                if (value > 0)
+                {
+                    _bulletCount = value;
+                }
+            }
+        }
+
+        public int DischargeTime
+        {
+            get { return _dischargeTime; }
+            set
+            {
+                if (value > 0)
+                {
+                    _dischargeTime = value;
+                }
+            }
+        }
+
+        public bool  IsAuto
+        {
+            get { return _isAuto; }
+            set
+            {
+                _isAuto = value;
+            }
+        }
+
+        public Weapon(int bulletcapacity, int bulletcount , int dischargetime, bool isauto)
+        {
+            BulletCapacity = bulletcapacity;
+            BulletCount = bulletcount;
+            DischargeTime = dischargetime;
+            IsAuto = isauto;
+        }
+
+
+        public void Shoot()
+        {
+            if (BulletCount!=0)
+            {
+                Console.WriteLine("Gulle atildi");
+                BulletCount--;
+                if (BulletCount<=1)
+                {
+                    Console.WriteLine("Daraqda gulle bitdi. Yeniden Gulle doldurulur...");
+                    Reload();
+                }
+
+        }
+         }
+
+        public void Fire()
+        {
+            if (IsAuto!=true)
+            {
+                if (BulletCount != 0)
+                {
+                    Console.WriteLine("Tekli modda atis edildi: ");
+                    BulletCount--;
+                }
+                else
+                {
+                    Console.WriteLine("Daraqda gulleniz yoxdur!");
+                }
+            }
+            else
+            {
+                if (BulletCount != 0)
+                {
+                    int fireTime = DischargeTime / BulletCapacity;
+                    Console.WriteLine($"Daraqdaki ggulleler ateshlendi ve : {BulletCount * fireTime} saniyede bitdi ");
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Daraqda gulleniz yoxdur!");
+                }
+            }
+        }
+
+
+        public int GetRemainBulletCount()
+        {
+             int remainBullet = (BulletCapacity - BulletCount);
+             return remainBullet;
+        }
+
+
+        public void Reload()
+        {
+            if (BulletCount!=BulletCapacity)
+            {
+                BulletCount = BulletCount + GetRemainBulletCount();
+                Console.WriteLine("Daraq tam doldu.");
+            }
+        }
+
+        public void ChangeFireMode()
+        {
+            
+            if(IsAuto)
+            {
+                IsAuto = false;
+                Console.WriteLine("Mod deyishdi Tekli moda kechildi");
+            }
+            else
+            {
+                IsAuto = true;
+                Console.WriteLine("Mode deyishdi Auto moda kechildi.");
+            }
+        }
+
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Bullet Capacity: {BulletCapacity}\nBullet Count: {BulletCount}\nDischarge Time: {DischargeTime}");
+            if (IsAuto)
+            {
+                Console.WriteLine("Auto");
+            }
+            else
+            {
+                Console.WriteLine("Tekli");
+            }
+        }
+
+
+
+    }
+}
+
+   
---
+++ WeaponTask/WeaponTask/Program.cs
@@ -0,0 +1,58 @@
+﻿using WeaponTask.Models;
+
+namespace WeaponTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {           
+            Weapon weapon = new Weapon(5, 2, 30, true);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("|----------------   User Menu   ----------------|");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(" 0)Informasiya almaq \n 1)Shoot metodu \n 2)Fire metodu \n 3)GetRemainBulletCount metodu \n 4)Reload metodu \n 5)ChangeFireMode metodu \n 6)Proqramdan dayandırmaq");
+            bool repeat = false;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("|-----------------------------------------------|");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("Your Choice : ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 0:
+                        weapon.GetInfo();
+                        break;
+                                              
+                    case 1:
+                        weapon.Shoot();
+                        break;
+                    case 2:
+                        weapon.Fire();
+                        break;
+                    case 3:
+                        Console.WriteLine("Daragin dolmasi ucun lazim olan gulle sayi " + weapon.GetRemainBulletCount()); 
+                        break;
+                    case 4:
+                        weapon.Reload();
+                        break;
+                    case 5:
+                        weapon.ChangeFireMode();
+                        break;
+                    case 6:
+                        Console.WriteLine("Proramdan cixish edildi");
+                        break;
+                    case 7:
+                        repeat = true;
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Choice");
+                        break;
+                }
+            } while (!repeat);
+        }
+
+    }
+
+}
